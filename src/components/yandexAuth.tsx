7e1c1d0aaa469604b64c1e324889a82bab0ee523
d.chernyshev <DChenyshev@sbercity.ru>
2024-06-02 00:14:23 +0300

    yandex auth

--- src/components/yandexAuth.tsx
+++ src/components/yandexAuth.tsx
@@ -2,23 +2,20 @@ import { useEffect } from 'react';
 
 export const YandexAuth = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js';
-    // script.onload = () => {
-    //   window.YaAuthSuggest.init(
-    //     {
-    //       client_id: 'e7fe8ce8e9ff4eb9b282ed7a6a697398',
-    //       response_type: 'token',
-    //       redirect_uri: 'http://localhost:3000'
-    //     },
-    //     'http://localhost',
-    //     { view: 'default' }
-    //   )
-    //     .then(({ handler }) => handler())
-    //     .then((data) => console.log('Сообщение с токеном', data))
-    //     .catch((error) => console.log('Обработка ошибки', error));
-    // };
-    document.body.appendChild(script);
+    document.addEventListener('load', () => {
+      window.YaAuthSuggest.init(
+        {
+          client_id: 'e7fe8ce8e9ff4eb9b282ed7a6a697398',
+          response_type: 'token',
+          redirect_uri: 'http://localhost:3000'
+        },
+        'http://localhost:3000',
+        { view: 'default' }
+      )
+        .then(({ handler }) => handler())
+        .then((data) => console.log('Сообщение с токеном', data))
+        .catch((error) => console.log('Обработка ошибки', error));
+    });
   }, []);
 
   return <>1234</>;
